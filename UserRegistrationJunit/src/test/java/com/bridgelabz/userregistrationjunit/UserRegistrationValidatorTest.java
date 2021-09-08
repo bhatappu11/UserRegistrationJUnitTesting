59e4e5de9ecbf9554b7bfa156bbb6513f09fb8fe
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-08 23:03:19 +0530

    ADDED: test case for validating firstName

--- UserRegistrationJunit/src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationValidatorTest.java
+++ UserRegistrationJunit/src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationValidatorTest.java
@@ -1,5 +1,27 @@
 package com.bridgelabz.userregistrationjunit;
 
-public class UserRegistrationValidatorTest {
+import org.junit.Test;
+import org.junit.Assert;
 
+public class UserRegistrationValidatorTest {
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		UserRegistrationValidator validator = new UserRegistrationValidator();	
+		boolean result = validator.checkFirstName("Arpitha");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
+		UserRegistrationValidator validator = new UserRegistrationValidator();	
+		boolean result = validator.checkFirstName("Ar");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenUpperCaseMissing_ShouldReturnFalse() {
+		UserRegistrationValidator validator = new UserRegistrationValidator();	
+		boolean result = validator.checkFirstName("arpitha");
+		Assert.assertFalse(result);
+	}
 }
