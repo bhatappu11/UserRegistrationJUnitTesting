ef9205c45f3798dea835219dbf9ae39d55708fb9
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-11 00:27:58 +0530

    MODIFIED: added try-catch blocks to throw custom exception

--- UserRegistrationJunit/src/main/java/com/bridgelabz/userregistrationjunit/UserRegistrationValidator.java
+++ UserRegistrationJunit/src/main/java/com/bridgelabz/userregistrationjunit/UserRegistrationValidator.java
@@ -3,41 +3,74 @@ package com.bridgelabz.userregistrationjunit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.bridgelabz.userregistrationjunit.UserRegistrationException.ExceptionType;
+
 public class UserRegistrationValidator {
 	
-	static boolean checkPassword(String password) {
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9]).{8,}");
-		Matcher matcher = pattern.matcher(password);
-		if(!matcher.matches()) 
-			return false;
-		pattern = Pattern.compile("^[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$");
-		matcher = pattern.matcher(password);
-		return matcher.matches();
+	static boolean checkPassword(String password) throws UserRegistrationException {
+		try {
+			if(password.length()==0) throw new UserRegistrationException(ExceptionType.ENTERED_EMPTY, "Password cannot be empty");
+			Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9]).{8,}");
+			Matcher matcher = pattern.matcher(password);
+			if(!matcher.matches()) 
+				return false;
+			pattern = Pattern.compile("^[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$");
+			matcher = pattern.matcher(password);
+			if(matcher.matches()) {
+				return true;
+			} else {
+				throw new UserRegistrationException(ExceptionType.ENTERED_INVALID, "Password is invalid");
+			}
+		}catch(NullPointerException e) {
+			throw new UserRegistrationException(ExceptionType.ENTERED_NULL, "Password cannot be null");
+		}
 	}
 		
 
-	static boolean checkMobileNumber(String number) {
-		Pattern pattern = Pattern.compile("[1-9][0-9][\\s][1-9][0-9]{9}");
-		Matcher matcher = pattern.matcher(number);
-		return matcher.matches();				
+	static boolean checkMobileNumber(String number) throws UserRegistrationException {
+		try {
+			if(number.length()==0) throw new UserRegistrationException(ExceptionType.ENTERED_EMPTY, "Mobile number cannot be empty");
+			Pattern pattern = Pattern.compile("[1-9][0-9][\\s][1-9][0-9]{9}");
+			Matcher matcher = pattern.matcher(number);
+			if(matcher.matches()) {
+				return true;
+			} else {
+				throw new UserRegistrationException(ExceptionType.ENTERED_INVALID, "Mobile number is invalid");
+			}
+		}catch(NullPointerException e) {
+			throw new UserRegistrationException(ExceptionType.ENTERED_NULL, "Mobile number cannot be null");
+		}
 	}
 
-	static boolean checkEmail(String email) {
-		Pattern pattern = Pattern.compile("^abc[a-z0-9]*([+\\-_.][a-z0-9]{3})?@[a-z0-9]+\\.[a-z]{2,3}(\\.[a-z]{2,3})?$");
-		Matcher matcher = pattern.matcher(email);
-		return matcher.matches();
+	static boolean checkEmail(String email) throws UserRegistrationException {
+		try {
+			if(email.length()==0) throw new UserRegistrationException(ExceptionType.ENTERED_EMPTY, "Email cannot be empty");
+			Pattern pattern = Pattern.compile("^abc[a-z0-9]*([+\\-_.][a-z0-9]{3})?@[a-z0-9]+\\.[a-z]{2,3}(\\.[a-z]{2,3})?$");
+			Matcher matcher = pattern.matcher(email);
+			if(matcher.matches()) {
+				return true;
+			} else {
+				throw new UserRegistrationException(ExceptionType.ENTERED_INVALID, "Email is invalid");
+			}
+		}catch(NullPointerException e) {
+			throw new UserRegistrationException(ExceptionType.ENTERED_NULL, "Email cannot be null");
+		}
 	}
 
-	static boolean checkLastName(String lastName) {
-		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
-		Matcher matcher = pattern.matcher(lastName);
-		return matcher.matches();
-	}
 
-	static boolean checkFirstName(String firstName) {
-		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
-		Matcher matcher = pattern.matcher(firstName);
-		return matcher.matches();
+	static boolean checkName(String name) throws UserRegistrationException {
+		try {
+			if(name.length()==0) throw new UserRegistrationException(ExceptionType.ENTERED_EMPTY, "Name cannot be empty");
+			Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+			Matcher matcher = pattern.matcher(name);
+			if(matcher.matches()) {
+				return true;
+			} else {
+				throw new UserRegistrationException(ExceptionType.ENTERED_INVALID, "Name is invalid");
+			}
+		}catch(NullPointerException e) {
+			throw new UserRegistrationException(ExceptionType.ENTERED_NULL, "Name cannot be null");
+		}
 	}
 
 }
